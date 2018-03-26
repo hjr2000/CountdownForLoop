8e26d86f38c6ddc78623f66fae6392d5ef2473f9
Haden Jackson-Robbins <haden.jackson-robbins@asos.com>
2018-03-26 14:14:13 +0100

    One bug removed after running HackerRank test case 1, but at least one more remains

--- CountdownForLoop/Program.cs
+++ CountdownForLoop/Program.cs
@@ -12,49 +12,116 @@ namespace CountdownForLoop
             ////////////////////////////////////////////////////////////////////////////////
             // Hackerrank Test Case 1
             ////////////////////////////////////////////////////////////////////////////////
-            //var scores_raw = "295 294 291 287 287 285 285 284 283 279 277 274 274 271 270 268 268 268 264 260 259 258 257 255 252 250 244 241 240 237 236 236 231 227 227 227 226 225 224 223 216 212 200 197 196 194 193 189 188 187 183 182 178 177 173 171 169 165 143 140 137 135 133 130 130 130 128 127 122 120 116 114 113 109 106 103 99 92 85 81 69 68 63 63 63 61 57 51 47 46 38 30 28 25 22 15 14 12 6 4";
-            //string[] scores_temp = scores_raw.Split(' ');
-            //int[] scores = Array.ConvertAll(scores_temp, Int32.Parse);
+            Debug.WriteLine("-------------------------------------------------------------------");
+            Debug.WriteLine("Hackerrank Test Case 1");
+            var scores_raw = "295 294 291 287 287 285 285 284 283 279 277 274 274 271 270 268 268 268 264 260 259 258 257 255 252 250 244 241 240 237 236 236 231 227 227 227 226 225 224 223 216 212 200 197 196 194 193 189 188 187 183 182 178 177 173 171 169 165 143 140 137 135 133 130 130 130 128 127 122 120 116 114 113 109 106 103 99 92 85 81 69 68 63 63 63 61 57 51 47 46 38 30 28 25 22 15 14 12 6 4";
+            string[] scores_temp = scores_raw.Split(' ');
+            int[] scoresTC1 = Array.ConvertAll(scores_temp, Int32.Parse);
 
-            //var alice_raw = "5 5 6 14 19 20 23 25 29 29 30 30 32 37 38 38 38 41 41 44 45 45 47 59 59 62 63 65 67 69 70 72 72 76 79 82 83 90 91 92 93 98 98 100 100 102 103 105 106 107 109 112 115 118 118 121 122 122 123 125 125 125 127 128 131 131 133 134 139 140 141 143 144 144 144 144 147 150 152 155 156 160 164 164 165 165 166 168 169 170 171 172 173 174 174 180 184 187 187 188 194 197 197 197 198 201 202 202 207 208 211 212 212 214 217 219 219 220 220 223 225 227 228 229 229 233 235 235 236 242 242 245 246 252 253 253 257 257 260 261 266 266 268 269 271 271 275 276 281 282 283 284 285 287 289 289 295 296 298 300 300 301 304 306 308 309 310 316 318 318 324 326 329 329 329 330 330 332 337 337 341 341 349 351 351 354 356 357 366 369 377 379 380 382 391 391 394 396 396 400";
-            //string[] alice_temp = alice_raw.Split(' ');
-            //int[] alice = Array.ConvertAll(alice_temp, Int32.Parse);
+            var alice_raw = "5 5 6 14 19 20 23 25 29 29 30 30 32 37 38 38 38 41 41 44 45 45 47 59 59 62 63 65 67 69 70 72 72 76 79 82 83 90 91 92 93 98 98 100 100 102 103 105 106 107 109 112 115 118 118 121 122 122 123 125 125 125 127 128 131 131 133 134 139 140 141 143 144 144 144 144 147 150 152 155 156 160 164 164 165 165 166 168 169 170 171 172 173 174 174 180 184 187 187 188 194 197 197 197 198 201 202 202 207 208 211 212 212 214 217 219 219 220 220 223 225 227 228 229 229 233 235 235 236 242 242 245 246 252 253 253 257 257 260 261 266 266 268 269 271 271 275 276 281 282 283 284 285 287 289 289 295 296 298 300 300 301 304 306 308 309 310 316 318 318 324 326 329 329 329 330 330 332 337 337 341 341 349 351 351 354 356 357 366 369 377 379 380 382 391 391 394 396 396 400";
+            string[] alice_temp = alice_raw.Split(' ');
+            int[] aliceTC1 = Array.ConvertAll(alice_temp, Int32.Parse);
+
+            var expectedResult_raw = "88 88 87 85 84 84 83 82 81 81 80 80 80 80 79 79 79 79 79 79 79 79 77 75 75 74 73 73 73 71 71 71 71 71 71 70 70 69 69 68 68 68 68 67 67 67 66 66 65 65 64 64 62 61 61 60 59 59 59 59 59 59 58 57 56 56 55 55 53 52 52 51 51 51 51 51 51 51 51 51 51 51 51 51 50 50 50 50 49 49 48 48 47 47 47 45 43 42 42 41 38 36 36 36 36 35 35 35 35 35 35 34 34 34 33 33 33 33 33 32 30 28 28 28 28 27 27 27 26 23 23 22 22 20 20 20 18 18 15 15 14 14 13 13 11 11 10 10 8 8 7 6 5 4 4 4 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1";
+            string[] expectedResult_temp = expectedResult_raw.Split(' ');
+            int[] expectedResultTC1 = Array.ConvertAll(expectedResult_temp, Int32.Parse);
+
+            int[][] aliceScoresArrayOfArraysTC1 = new int[][]
+            {
+                aliceTC1
+            };
+
+            int[][] expectedResultsArrayOfArraysTC1 = new int[][]
+            {
+                expectedResultTC1
+            };
+
+            RunTests(scoresTC1, aliceScoresArrayOfArraysTC1, expectedResultsArrayOfArraysTC1);
+
+            ////////////////////////////////////////////////////////////////////////////////
+            // Hackerrank Test Case 1 subset
+            ////////////////////////////////////////////////////////////////////////////////
+            Debug.WriteLine("-------------------------------------------------------------------");
+            Debug.WriteLine("Hackerrank Test Case 1S");
+
+            var scores_rawTC1S = "295 294 291 287 287 285 285 284 283 279 277 274 274 271 270 268 268 268 264 260 259 258 257 255 252 250 244 241 240 237 236 236 231 227 227 227 226 225 224 223 216 212 200 197 196 194 193 189 188 187 183 182 178 177 173 171 169 165 143 140 137 135 133 130 130 130 128 127 122 120 116 114 113 109 106 103 99 92 85 81 69 68 63 63 63 61 57 51 47 46 38 30 28 25 22 15 14 12 6 4";
+            string[] scores_tempTC1S = scores_rawTC1S.Split(' ');
+            int[] scoresTC1S = Array.ConvertAll(scores_tempTC1S, Int32.Parse);
+
+            var alice_rawTC1S = "275 276 281 282 283 284 285 287 289 289";
+            string[] alice_tempTC1S = alice_rawTC1S.Split(' ');
+            int[] aliceTC1S = Array.ConvertAll(alice_tempTC1S, Int32.Parse);
+
+            var expectedResult_rawTC1S = "10 10 8 8 7 6 5 4 4 4";
+            string[] expectedResult_tempTC1S = expectedResult_rawTC1S.Split(' ');
+            int[] expectedResultTC1S = Array.ConvertAll(expectedResult_tempTC1S, Int32.Parse);
+
+            int[][] aliceScoresArrayOfArraysTC1S = new int[][]
+            {
+                aliceTC1S
+            };
+
+            int[][] expectedResultsArrayOfArraysTC1S = new int[][]
+            {
+                expectedResultTC1S
+            };
+
+            RunTests(scoresTC1S, aliceScoresArrayOfArraysTC1S, expectedResultsArrayOfArraysTC1S);
 
             ////////////////////////////////////////////////////////////////////////////////
             // HJR Test set 1
             ////////////////////////////////////////////////////////////////////////////////            
             Debug.WriteLine("-------------------------------------------------------------------");
-            int[] scores = { 100, 100, 50, 40, 40, 20, 10 };
+            Debug.WriteLine("HJR Test Cases");
+            int[] scoresHJR = { 100, 100, 50, 40, 40, 20, 10 };
 
             int[] alice0 = { 5, 25, 50, 120 };
             int[] expectedResult0 = { 6, 4, 2, 1 };
-            int[] alice1 = { 10, 20, 50, 120 };  
+            int[] alice1 = { 10, 20, 50, 120 };
             int[] expectedResult1 = { 5, 4, 2, 1 };
-            int[] alice2 = { 10, 20, 115, 120 };  
+            int[] alice2 = { 10, 20, 115, 120 };
             int[] expectedResult2 = { 5, 4, 1, 1 };
-            int[] alice3 = { 10, 20, 120, 120 }; 
+            int[] alice3 = { 10, 20, 120, 120 };
             int[] expectedResult3 = { 5, 4, 1, 1 };
-            int[] alice4 = { 2, 5, 100, 120 };   
+            int[] alice4 = { 2, 5, 100, 120 };
             int[] expectedResult4 = { 6, 6, 1, 1 };
-            int[] alice5 = { 2, 75, 75, 120 }; 
+            int[] alice5 = { 2, 75, 75, 120 };
             int[] expectedResult5 = { 6, 2, 2, 1 };
             int[] alice6 = { 10, 30, 75, 75 };
             int[] expectedResult6 = { 5, 4, 2, 2 };
+            int[] alice7 = { 2, 3, 10, 30, 75, 75, 100 };
+            int[] expectedResult7 = { 6, 6, 5, 4, 2, 2, 1 };
 
-            int[][] aliceScoresArrayOfArrays = new int[][]
+            int[][] aliceScoresArrayOfArraysHJR = new int[][]
             {
-                alice0, alice1, alice2, alice3, alice4, alice5, alice6
+                alice0, alice1, alice2, alice3, alice4, alice5, alice6, alice7
             };
 
-            int[][] expectedResultsArrayOfArrays = new int[][]
+            //  int[][] aliceScoresArrayOfArrays = new int[][]
+            //{
+            //      alice7
+            //};
+
+            int[][] expectedResultsArrayOfArraysHJR = new int[][]
             {
-                expectedResult0, expectedResult1, expectedResult2, expectedResult3, expectedResult4, expectedResult5, expectedResult6
+                expectedResult0, expectedResult1, expectedResult2, expectedResult3, expectedResult4, expectedResult5, expectedResult6, expectedResult7
             };
 
+            //          int[][] expectedResultsArrayOfArrays = new int[][]
+            //{
+            //              expectedResult7
+            //};
+
+            RunTests(scoresHJR, aliceScoresArrayOfArraysHJR, expectedResultsArrayOfArraysHJR);
+
+        }
+
+        private static void RunTests(int[] scores, int[][] aliceScoresArrayOfArrays, int[][] expectedResultsArrayOfArrays)
+        {
             int count = 0;
             foreach (int[] aliceScoreArray in aliceScoresArrayOfArrays)
             {
-                int[] result = ClimbingLeaderboard(scores, aliceScoreArray);
+                int[] result = climbingLeaderboard(scores, aliceScoreArray);
                 var expectedResultsArray = expectedResultsArrayOfArrays[count];
                 bool isEqual = Enumerable.SequenceEqual(result, expectedResultsArray);
                 if (isEqual)
@@ -62,14 +129,13 @@ namespace CountdownForLoop
                 else
                 {
                     Debug.WriteLine("**** " + count + " Test failed. Expected " + String.Join(" ", expectedResultsArray) + " but got " + String.Join(" ", result) + " ****");
-                }                    
+                }
                 count++;
             }
             Debug.WriteLine("-------------------------------------------------------------------");
-
         }
 
-        private static int[] ClimbingLeaderboard(int[] otherPeoplesScores, int[] aliceScores)
+        private static int[] climbingLeaderboard(int[] scores, int[] alice)
         {
             var aliceScoreRanking = new List<int>();
             var scoresStartingIndex = 0;
@@ -78,9 +144,9 @@ namespace CountdownForLoop
             var lastScore = -1;
             var finalScoreRankingFound = false;
 
-            for (int count = aliceScores.Length - 1; count > -1; count--)
+            for (int count = alice.Length - 1; count > -1; count--)
             {
-                var aliceScore = aliceScores[count];
+                var aliceScore = alice[count];
                 //Debug.WriteLine("---------------------------------------");
                 //Debug.WriteLine(count + ": Alice score " + aliceScore);
 
@@ -92,7 +158,13 @@ namespace CountdownForLoop
 
                 lastScore = aliceScore;
 
-                var score = otherPeoplesScores[scoresStartingIndex];
+                if (scoresStartingIndex == -1)
+                {
+                    aliceScoreRanking.Add(nextScoreRanking);
+                    continue;
+                }
+
+                var score = scores[scoresStartingIndex];
 
                 if (aliceScore > score)
                 {
@@ -101,17 +173,22 @@ namespace CountdownForLoop
                     lastScoreRanking = nextScoreRanking;
 
                     // Find next valid index (i.e. scoresStartingIndex)
-                    if (scoresStartingIndex != 0)
+                    if (scoresStartingIndex != 0 && count > 0)
                     {
-                        scoresStartingIndex = FindNextUniqueScoreIndex(scoresStartingIndex, otherPeoplesScores);
-                        // Increase nextScoreRanking 
-                        nextScoreRanking++;
+
+                        var nextAliceScore = alice[count -1];
+                        if (!(nextAliceScore > score))
+                        {
+                            scoresStartingIndex = FindNextUniqueScoreIndex(scoresStartingIndex, scores);
+                            // Increase nextScoreRanking 
+                            nextScoreRanking++;
+                        }                            
                     }
                 }
                 else if (aliceScore < score)
                 {
                     // See if we're at the end of the list of other's scores
-                    var nextUniqueScoreIndex = FindNextUniqueScoreIndex(scoresStartingIndex, otherPeoplesScores);
+                    var nextUniqueScoreIndex = FindNextUniqueScoreIndex(scoresStartingIndex, scores);
                     if (nextUniqueScoreIndex == -1)
                     {
                         nextScoreRanking++;
@@ -139,10 +216,10 @@ namespace CountdownForLoop
                     // Deal with the situation where we're at the end of the Alice score's array, or the final score will not be ranked
                     if (count == 0)
                     {
-                        CountZeroEndGame(otherPeoplesScores, aliceScoreRanking, scoresStartingIndex, ref nextScoreRanking, aliceScore, ref score, ref lastScoreRanking, finalScoreRankingFound);
+                        CountZeroEndGame(scores, aliceScoreRanking, scoresStartingIndex, ref nextScoreRanking, aliceScore, ref score, ref lastScoreRanking, finalScoreRankingFound);
                     }
                     else
-                        FindNextImportantScore(otherPeoplesScores, ref scoresStartingIndex, aliceScore, ref aliceScoreRanking, ref nextScoreRanking, count, ref finalScoreRankingFound, ref lastScoreRanking);
+                        FindNextImportantScore(ref alice, scores, ref scoresStartingIndex, aliceScore, ref aliceScoreRanking, ref nextScoreRanking, count, ref finalScoreRankingFound, ref lastScoreRanking);
                 }
                 else
                 {
@@ -153,7 +230,7 @@ namespace CountdownForLoop
                     lastScoreRanking = nextScoreRanking;
 
                     // Find next valid index (i.e. scoresStartingIndex)
-                    scoresStartingIndex = FindNextUniqueScoreIndex(scoresStartingIndex, otherPeoplesScores);
+                    scoresStartingIndex = FindNextUniqueScoreIndex(scoresStartingIndex, scores);
 
                     // Increase nextScoreRanking 
                     nextScoreRanking++;
@@ -202,7 +279,7 @@ namespace CountdownForLoop
             }
         }
 
-        private static void FindNextImportantScore(int[] otherPeoplesScores, ref int scoresStartingIndex, int aliceScore, ref List<int> aliceScoreRanking, ref int nextScoreRanking, int count, ref bool finalScoreRankingFound, ref int lastScoreRanking)
+        private static void FindNextImportantScore(ref int[]alice, int[] otherPeoplesScores, ref int scoresStartingIndex, int aliceScore, ref List<int> aliceScoreRanking, ref int nextScoreRanking, int count, ref bool finalScoreRankingFound, ref int lastScoreRanking)
         {
             var score = otherPeoplesScores[scoresStartingIndex];
 
@@ -212,11 +289,16 @@ namespace CountdownForLoop
                 aliceScoreRanking.Add(nextScoreRanking);
                 lastScoreRanking = nextScoreRanking;
 
-                // Find next valid index (i.e. scoresStartingIndex)
-                scoresStartingIndex = FindNextUniqueScoreIndex(scoresStartingIndex, otherPeoplesScores);
+                var nextAliceScore = alice[count - 1];
+                if (!(nextAliceScore > score))
+                {
+                    // Find next valid index (i.e. scoresStartingIndex)
+                    scoresStartingIndex = FindNextUniqueScoreIndex(scoresStartingIndex, otherPeoplesScores);
 
-                // Increase nextScoreRanking 
-                nextScoreRanking++;
+                    // Increase nextScoreRanking 
+                    nextScoreRanking++;
+                }
+
             }
             else if (aliceScore < score)
             {
