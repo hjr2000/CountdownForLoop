0ba13b9a9f06b5f90c2fba5d9f2932a4566b1940
Haden Jackson-Robbins <haden.jackson-robbins@asos.com>
2018-03-26 17:13:07 +0100

    More work to fix an issue with test case 1

--- CountdownForLoop/Program.cs
+++ CountdownForLoop/Program.cs
@@ -36,23 +36,25 @@ namespace CountdownForLoop
                 expectedResultTC1
             };
 
-            RunTests(scoresTC1, aliceScoresArrayOfArraysTC1, expectedResultsArrayOfArraysTC1);
+            //RunTests(scoresTC1, aliceScoresArrayOfArraysTC1, expectedResultsArrayOfArraysTC1);
 
             ////////////////////////////////////////////////////////////////////////////////
             // Hackerrank Test Case 1 subset
             ////////////////////////////////////////////////////////////////////////////////
             Debug.WriteLine("-------------------------------------------------------------------");
             Debug.WriteLine("Hackerrank Test Case 1S");
-
+            
+            // ranking                                                                         13
+            //index                                                                    13      15    
             var scores_rawTC1S = "295 294 291 287 287 285 285 284 283 279 277 274 274 271 270 268 268 268 264 260 259 258 257 255 252 250 244 241 240 237 236 236 231 227 227 227 226 225 224 223 216 212 200 197 196 194 193 189 188 187 183 182 178 177 173 171 169 165 143 140 137 135 133 130 130 130 128 127 122 120 116 114 113 109 106 103 99 92 85 81 69 68 63 63 63 61 57 51 47 46 38 30 28 25 22 15 14 12 6 4";
             string[] scores_tempTC1S = scores_rawTC1S.Split(' ');
             int[] scoresTC1S = Array.ConvertAll(scores_tempTC1S, Int32.Parse);
 
-            var alice_rawTC1S = "275 276 281 282 283 284 285 287 289 289";
+            var alice_rawTC1S = "227 228 229 229 233 235 235 236 242 242 245 246 252 253 253 257 257 260 261 266 266 268 269 271 271 275 276 281 282 283 284 285 287 289 289";
             string[] alice_tempTC1S = alice_rawTC1S.Split(' ');
             int[] aliceTC1S = Array.ConvertAll(alice_tempTC1S, Int32.Parse);
 
-            var expectedResult_rawTC1S = "10 10 8 8 7 6 5 4 4 4";
+            var expectedResult_rawTC1S = "28 28 28 28 27 27 27 26 23 23 22 22 20 20 20 18 18 15 15 14 14 13 13 11 11 10 10 8 8 7 6 5 4 4 4";
             string[] expectedResult_tempTC1S = expectedResult_rawTC1S.Split(' ');
             int[] expectedResultTC1S = Array.ConvertAll(expectedResult_tempTC1S, Int32.Parse);
 
@@ -128,7 +130,7 @@ namespace CountdownForLoop
                     Debug.WriteLine(count + " Test passed");
                 else
                 {
-                    Debug.WriteLine("**** " + count + " Test failed. Expected " + String.Join(" ", expectedResultsArray) + " but got " + String.Join(" ", result) + " ****");
+                    Debug.WriteLine("**** " + count + " Test failed. Expected \n" + String.Join(" ", expectedResultsArray) + "\n" + String.Join(" ", result) + " ****");
                 }
                 count++;
             }
@@ -290,7 +292,7 @@ namespace CountdownForLoop
                 lastScoreRanking = nextScoreRanking;
 
                 var nextAliceScore = alice[count - 1];
-                if (!(nextAliceScore > score))
+                if (!(nextAliceScore >= score))
                 {
                     // Find next valid index (i.e. scoresStartingIndex)
                     scoresStartingIndex = FindNextUniqueScoreIndex(scoresStartingIndex, otherPeoplesScores);
@@ -298,7 +300,6 @@ namespace CountdownForLoop
                     // Increase nextScoreRanking 
                     nextScoreRanking++;
                 }
-
             }
             else if (aliceScore < score)
             {
