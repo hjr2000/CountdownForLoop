175b144fcaa74a78b306bf40c41fd84227a8422c
hjr2000 <hjr2000@users.noreply.github.com>
2018-03-27 10:08:58 +0100

    Hackerrank test case 1 fixed. Only one hackerrank test failing now

--- CountdownForLoop/Program.cs
+++ CountdownForLoop/Program.cs
@@ -36,7 +36,7 @@ namespace CountdownForLoop
                 expectedResultTC1
             };
 
-            //RunTests(scoresTC1, aliceScoresArrayOfArraysTC1, expectedResultsArrayOfArraysTC1);
+            RunTests(scoresTC1, aliceScoresArrayOfArraysTC1, expectedResultsArrayOfArraysTC1);
 
             ////////////////////////////////////////////////////////////////////////////////
             // Hackerrank Test Case 1 subset
@@ -45,7 +45,7 @@ namespace CountdownForLoop
             Debug.WriteLine("Hackerrank Test Case 1S");
             
             // ranking                                                                         13
-            //index                                                                    13      15    
+            //index                                                                    13      15                                                                     33
             var scores_rawTC1S = "295 294 291 287 287 285 285 284 283 279 277 274 274 271 270 268 268 268 264 260 259 258 257 255 252 250 244 241 240 237 236 236 231 227 227 227 226 225 224 223 216 212 200 197 196 194 193 189 188 187 183 182 178 177 173 171 169 165 143 140 137 135 133 130 130 130 128 127 122 120 116 114 113 109 106 103 99 92 85 81 69 68 63 63 63 61 57 51 47 46 38 30 28 25 22 15 14 12 6 4";
             string[] scores_tempTC1S = scores_rawTC1S.Split(' ');
             int[] scoresTC1S = Array.ConvertAll(scores_tempTC1S, Int32.Parse);
@@ -179,7 +179,7 @@ namespace CountdownForLoop
                     {
 
                         var nextAliceScore = alice[count -1];
-                        if (!(nextAliceScore > score))
+                        if (!(nextAliceScore >= score))
                         {
                             scoresStartingIndex = FindNextUniqueScoreIndex(scoresStartingIndex, scores);
                             // Increase nextScoreRanking 
